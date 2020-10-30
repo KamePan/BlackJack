bcb4c26f41acfc9bac8a0a9d59f6c685f2402867
林辰育 <1980055813@qq.com>
2020-10-30 15:53:42 +0800

    Dealer and Player (80%)

--- src/Player.java
+++ src/Player.java
@@ -29,10 +29,33 @@ public class Player {
         hand = new Hand();
     }
 
+    public void show(){
+        System.out.print("该玩家的手牌为");
+        for(int i=0;i<hand.getLength();i++){
+            System.out.print((i==0?":":";")+hand.get(i));
+        }
+        System.out.println();
+    }
+
+    public int getValue(){
+        return hand.getValue();
+    }
+
+    public boolean check(){
+        return getValue()<22;
+    }
+
     public int bet(){
-        System.out.println("请输入你的赌注");
-        Scanner scanner=new Scanner(System.in);
-        return scanner.nextInt();
+        System.out.println("当前余额为： " + player.getBalance());
+        System.out.println("请输入要投注的金额： ");
+        Scanner scanner = new Scanner(System.in);
+        Integer bet = scanner.nextInt();
+        while (true){
+            if (bet > player.getBalance())
+                System.out.println("投注金额超过余额，请重新输入");
+            else break;
+        }
+        return bet;
     }
 
     public void firstlyGetCards(){
@@ -40,7 +63,9 @@ public class Player {
         hand.getOne();
     }
 
-    public void getOne(){
-
+    public void getOneCard(){
+        hand.getOne();
     }
+
+
 }
